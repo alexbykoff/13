df9d83175bb0b0f5deb89063632ba56b1af941b4
tomkallen <tom9433021>
2017-08-14 20:56:21 +0300

    walls with passage

--- js/builders.js
+++ js/builders.js
@@ -7,14 +7,29 @@ export function buildLevel() {
         for (let j = 0; j < 49; j++) {
             const cell = document.createElement("div");
             cell.className = "level-cell";
-            if (j < 7 || j > 41 || j % 7 === 0 || (j + 1) % 7 === 0) {
+            if (j > 41 || !((j + 1) % 7 ) && !needPass(i, j)) {
                 cell.classList.add("cell-wall");
             }
+
             cell.id = `cell-${i}-${j}`;
-            //cell.innerHTML = `${i}-${j}`;
+            cell.innerHTML = `${j}`;
             room.appendChild(cell);
         }
         levelWrapper.appendChild(room);
     }
 
 }
+
+function needPass(i, j) {
+    if (!((i + 1) % 3 === 0) && i < 6) {
+        if (j === 27 || j == 44) return true;
+    }
+    if (i > 5 && i !== 8) {
+        if (j == 27) return true;
+    }
+    if ((i + 1) % 3 === 0 && i !== 8) {
+        if (j == 44) return true;
+    }
+
+}
+

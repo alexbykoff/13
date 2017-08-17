c4ac9f5b9af438bb8b264b0d850775be628700c7
tomkallen <tom9433021>
2017-08-17 08:19:49 +0300

    few optimization tricks

--- js/helpers.js
+++ js/helpers.js
@@ -1,7 +1,8 @@
+export const $ = e => document.querySelector(e);
+
 export const rndInt = (min, max) => Math.floor(Math.random() * (max - min) + min);
 
-export const updateNeighbors = (row, column) => {
-  const neighbors = [
+export const getNearCells = (row, column) => [
     { x: row - 1, y: column },
     { x: row, y: column + 1 },
     { x: row + 1, y: column },
@@ -10,17 +11,18 @@ export const updateNeighbors = (row, column) => {
     { x: row + 1, y: column + 1 },
     { x: row - 1, y: column + 1 },
     { x: row + 1, y: column - 1 },
-  ];
+];
 
-  neighbors.forEach(cell => {
-    const div = document.querySelector(`#cell-${cell.x}-${cell.y}`);
-    if (div) div.classList.remove("fade");
+const updateNeighbours = (row, column) => {
+    getNearCells(row, column).forEach(cell => {
+    const div = $(`#cell-${cell.x}-${cell.y}`);
+    div && div.classList.remove("fade");
   });
-}
+};
 
 export const positionPlayer = (row, column, game) => {
-    const newPosition = document.querySelector(`#cell-${row}-${column}`);
-    const oldPosition = document.querySelector(`#cell-${game.playerPosition.row || 1}-${game.playerPosition.column || 1}`);
+    const newPosition = $(`#cell-${row}-${column}`);
+    const oldPosition = $(`#cell-${game.playerPosition.row || 1}-${game.playerPosition.column || 1}`);
 
     if ([...newPosition.classList].indexOf("free") >= 0) {
         oldPosition.classList.remove("player", "fade");
@@ -28,7 +30,7 @@ export const positionPlayer = (row, column, game) => {
         newPosition.classList.remove("free", "fade");
         newPosition.classList.add("player");
         game.playerPosition = Object.assign(game.playerPosition, {row, column});
-        updateNeighbors(row, column);
+        updateNeighbours(row, column);
     }
 };
 

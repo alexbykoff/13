c14d04023cf3c8018b619b532d8bf0fbe77ca0cb
tomkallen <tom9433021>
2017-08-19 22:24:33 +0300

    improvements

--- js/index.js
+++ js/index.js
@@ -1,16 +1,9 @@
 import Dungeon from "./Dungeon";
-import {buildWrapper, createNewRandomLevel, populateLevel} from "./builders";
-import {rndInt, positionPlayer} from "./helpers";
 import fx, {play} from "./sounds";
 
 const dungeon = new Dungeon(20);
 dungeon.initialize();
 dungeon.buildNewRoom(140);
-// game.level = buildWrapper();
-// createNewRandomLevel(140, game);
-// game.playerPosition = positionPlayer();
-//positionPlayer(game.playerPosition.row,, game);
-// populateLevel(game.level);
 play(fx.introSound);
 
 document.onkeyup = e => {
@@ -31,7 +24,7 @@ document.onkeyup = e => {
         default:
             break;
     }
-    positionPlayer(game.playerPosition.row + row, game.playerPosition.column + column, game);
+   // positionPlayer(game.playerPosition.row + row, game.playerPosition.column + column, game);
 };
 
 

4c4d32d8d282e9dfa97c96c205184514074b1aab
Irina Sokolovskaja <sokolovskaja.irina@gmail.com>
2017-08-29 22:44:39 +0300

    Create tooltip

--- js/Inventory.js
+++ js/Inventory.js
@@ -4,7 +4,7 @@ import {game} from "./index";
 export default class Inventory {
     constructor() {
         this.slots = 30;
-        this.visible =false
+        this.visible = false;
     }
 
     toggle() {
@@ -22,6 +22,17 @@ export default class Inventory {
             e.className = "inventory-object";
             e.style.backgroundImage = `url("${image}")`;
             inv.appendChild(e);
+            // show tooltip
+            e.addEventListener('mouseover', () => {
+              const t = document.createElement('div');
+              t.className = "inventory-tooltip";
+              t.innerHTML = `${item.name} ${item.rarity} ${item.slot} ${item.type}`;
+              e.appendChild(t);
+            });
+            // hide tooltip
+            e.addEventListener('mouseout', () => {
+              e.removeChild($(".inventory-tooltip"));
+            });
         });
         document.body.appendChild(inv);
         this.visible = true;
